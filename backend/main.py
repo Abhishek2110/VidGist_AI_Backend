eb5e0fcbd6b5009ce17f3424757981fc5269ce7a
Abhishek2110 <abhishekrajpal819@gmail.com>
2026-04-09 12:43:20 +0530

    Add Upload Video Endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "VidGist AI backend running"}
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/upload-video")
+async def upload_video(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+
+    return {
+        "filename": file.filename,
+        "path": file_path
+    }
