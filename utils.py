d1a8be9fbf30561e0280e2ba2a56704da22d8396
Abhishek2110 <abhishekrajpal819@gmail.com>
2026-04-13 12:16:33 +0530

    Updated code for audio extraction

--- utils.py
+++ utils.py
@@ -1,21 +1,11 @@
-import ffmpeg
 import os
 import whisper
 
 model = whisper.load_model("base")
 
 def extract_audio(video_path, audio_path):
-    try:
-        (
-            ffmpeg
-            .input(video_path)
-            .output(audio_path, format='mp3')
-            .run(overwrite_output=True)
-        )
-        return audio_path
-    except Exception as e:
-        print("Error extracting audio:", e)
-        return str(e)
+    # Temporary: skip extraction
+    return video_path
     
 def transcribe_audio(audio_path):
     try:
