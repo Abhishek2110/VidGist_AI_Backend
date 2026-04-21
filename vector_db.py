988920816ae47601a12b9e372380d539a0c1b0ab
Abhishek2110 <abhishekrajpal819@gmail.com>
2026-04-21 13:00:37 +0530

    Update create embeddings

--- vector_db.py
+++ vector_db.py
@@ -3,7 +3,7 @@ import uuid
 import time
 import requests
 from qdrant_client import QdrantClient
-from qdrant_client.models import VectorParams, Distance, PointStruct
+from qdrant_client.models import VectorParams, Distance, PointStruct, PayloadSchemaType
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -71,7 +71,12 @@ def create_collection():
             collection_name=COLLECTION_NAME,
             vectors_config=VectorParams(size=384, distance=Distance.COSINE),
         )
-
+    
+    client.create_payload_index(
+        collection_name=COLLECTION_NAME,
+        field_name="video_id",
+        field_schema=PayloadSchemaType.KEYWORD
+    )
 
 def store_embeddings(chunks, video_id):
     points = []
